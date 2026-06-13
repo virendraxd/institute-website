a3ba6eaa607d8e171d9b85b5b7c0461fc249b02e
virendraxd <shakyaraju605@gmail.com>
2026-06-13 18:20:03 +0530

    minor responsive changes

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -20,8 +20,9 @@ function About() {
 
         <Divider />
 
-        <div className="flex flex-col mt-12 gap-12 lg:gap-18">
-            <div className="flex flex-col justify-around">
+        <div className="flex flex-col mt-12 gap-12 lg:gap-18 ">
+          <div className="flex flex-col lg:flex-row justify-around gap-4 lg:gap-8">
+            <div className="flex flex-col justify-center gap-2"> 
               <p className="font-(family-name:--font-amethysta) text-4xl md:text-4xl leading-tight text-(--primary-accent)">
                 {instituteData[0].tagline}
               </p>
@@ -29,15 +30,18 @@ function About() {
               <p className="text-lg sm:text-1xl text-gray-600 mt-6">
                 {instituteData[0].description.replace('{name}', instituteData[0].name)}
               </p>
-
-            <img
-              src={instituteData[0].image} alt="teacher-image"
-              className="rounded-2xl w-full sm:h-92 max-h-128 object-cover shadow-xl mt-8 border-2 border-(--secondary-accent)"
-            />
             </div>
 
+            <div className="flex">
+              <img
+                src={instituteData[0].image} alt="teacher-image"
+                className="rounded-2xl w-full lg:w-540  max-h-128 object-cover shadow-xl mt-8 lg:mt-0 border-2 border-(--secondary-accent)"
+              />
+            </div>
+          </div>
+
           <div>
-            <div className="stats-grid grid grid-cols-2 sm:grid-cols-3 gap-6 pt-4">
+            <div className="stats-grid grid grid-cols-2 sm:grid-cols-3 gap-6 pt-4 text-center">
               <div className="stat-card p-4 md:p-6 bg-(--bg-secondary) border border-gray-100 rounded-2xl items-center">
                 <h3 className="text-4xl sm:text-5xl font-bold text-(--primary-accent)">
                   <AnimatedCounter end={15} suffix="+" />
