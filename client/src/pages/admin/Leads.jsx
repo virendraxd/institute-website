b120a135f0755b29486f6844812c42b5c8ae56c2
virendraxd <shakyaraju605@gmail.com>
2026-06-13 23:40:21 +0530

    better layout

--- client/src/pages/admin/Leads.jsx
+++ client/src/pages/admin/Leads.jsx
@@ -18,113 +18,121 @@ function Leads() {
         ["Course", selectedLead?.course],
         ["Parent", selectedLead?.parentName],
         ["School", selectedLead?.school],
-        ["Status", selectedLead?.status],
         ["Email", selectedLead?.email],
         ["Batch", selectedLead?.batchTiming],
+        ["Status", selectedLead?.status],
+        ["Form Type", selectedLead?.formType],
         ["Message", selectedLead?.message],
     ];
 
     return (
         <>
-            <section className='section-padding bg-(--bg-secondary)' >
+            <section className='px-2 py-8 sm:px-12 sm:y-16 bg-(--bg-secondary)'>
                 <h2 className='section-heading'>Leads</h2>
 
-                <div className="overflow-x-auto border-2 rounded-2xl border-gray-200 shadow-sm">
-                    <table border="1" className="bg-(--bg-color) w-full border-collapse">
-                        <thead className="bg-blue-100 border-b-2 border-gray-400">
-                            <tr>
-                                <th className="px-6 py-4 text-left font-semibold">Name</th>
-                                <th className="px-6 py-4 text-left font-semibold">Course</th>
-                                <th className="px-6 py-4 text-left font-semibold">Contact</th>
-                                <th className="px-6 py-4 text-left font-semibold">Status</th>
-                                <th className="px-6 py-4 text-left font-semibold">Date</th>
-                                <th className="px-6 py-4 text-left font-semibold">Action</th>
-                            </tr>
-                        </thead>
+                <div className="flex flex-col xl:flex-row gap-6 mt-4 sm:mt-10">
 
-                        <tbody>
-                            {leads.map((lead) => (
-                                <tr
-                                    key={lead._id}
-                                    className="border-b border-gray-200 hover:bg-gray-300/30 transition-colors"
-                                >
-                                    <td className="px-6 py-4 font-extrabold">{lead.name}</td>
-                                    <td className="px-6 py-4">{lead.course}</td>
-                                    <td className="px-6 py-4">{lead.phone}</td>
-
-                                    <td className="px-6 py-4">
-                                        <span className="px-3 py-1 rounded-full text-sm bg-blue-100 text-blue-700">
-                                            {lead.status}
-                                        </span>
-                                    </td>
-
-                                    <td>{new Date(lead.createdAt).toLocaleDateString()}</td>
-
-                                    <td>
-                                        <button
-                                            className="px-4 py-2 rounded-lg bg-(--primary-accent) text-white cursor-pointer"
-                                            onClick={() => setSelectedLead(lead)}
-                                        >
-                                            View
-                                        </button>
-                                    </td>
+                    {/* Table */}
+                    <div className="flex-1 overflow-x-auto border-2 rounded-xl border-gray-200 shadow-sm">
+                        <table border="1" className="bg-(--bg-color) w-full border-collapse">
+                            <thead className="bg-blue-100 border-b-2 border-gray-400">
+                                <tr>
+                                    <th className="px-6 py-4 text-left font-semibold">Name</th>
+                                    <th className="px-6 py-4 text-left font-semibold">Course</th>
+                                    <th className="px-6 py-4 text-left font-semibold">Contact</th>
+                                    <th className="px-6 py-4 text-left font-semibold">Status</th>
+                                    <th className="px-6 py-4 text-left font-semibold">Date</th>
+                                    <th className="px-6 py-4 text-left font-semibold">Action</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
 
-                <div className="bg-(--bg-color) px-6 py-8 w-96 border-2 rounded-r-2xl border-gray-200 shadow-sm">
-                    <h3 className="text-center font-bold text-2xl">Side Panel</h3>
+                            <tbody>
+                                {leads.map((lead) => (
+                                    <tr
+                                        key={lead._id}
+                                        className="border-b border-gray-200 hover:bg-gray-300/30 transition-colors"
+                                    >
+                                        <td className="px-6 py-4 font-extrabold">{lead.name}</td>
+                                        <td className="px-6 py-4">{lead.course}</td>
+                                        <td className="px-6 py-4">{lead.phone}</td>
 
-                    <table className="bg-(--bg-color) w-full shadow-sm mt-4">
-                        <thead className="bg-blue-100 border-b-2 border-gray-400">
-                            <tr>
-                                <th colSpan="2" className="text-left px-4 py-2 font-bold">
-                                    {selectedLead?.name || "Select a Lead"}
-                                </th>
-                            </tr>
-                        </thead>
+                                        <td className="px-6 py-4">
+                                            <span className="px-3 py-1 rounded-full text-sm bg-blue-100 text-blue-700">
+                                                {lead.status}
+                                            </span>
+                                        </td>
 
-                        <tbody>
-                            {leadFields.map(([label, value]) => (
-                                <tr key={label}>
-                                    <th className="text-left px-4 py-2 font-semibold">{label}</th>
-                                    <td>{value || "-"}</td>
-                                </tr>
-                            ))}
+                                        <td className="px-6 py-4">{new Date(lead.createdAt).toLocaleDateString()}</td>
 
-                        </tbody>
-                    </table>
-
-                    <div className="grid grid-cols-2 gap-3 mt-6">
-                        <a
-                            href={`https://wa.me/${selectedLead?.phone}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex-1 bg-green-600 text-white py-2 rounded-lg text-center cursor-pointer"
-                        >
-                            WhatsApp
-                        </a>
-
-                        <button
-                            className="flex-1 bg-yellow-500 text-white py-2 rounded-lg cursor-pointer"
-                        >
-                            Mark Contacted
-                        </button>
-
-                        <button
-                            className="flex-1 bg-blue-500 text-white py-2 rounded-lg cursor-pointer"
-                        >
-                            Mark Admitted
-                        </button>
-
-                        <button
-                            className="flex-1 bg-red-600 text-white py-2 rounded-lg cursor-pointer"
-                        >
-                            Delete
-                        </button>
+                                        <td className="px-6 py-4 text-left">
+                                            <button
+                                                className="px-6 py-3 rounded-lg bg-(--primary-accent) text-white hover:text-white/80 cursor-pointer hover:bg-(--primary-light) hover:scale"
+                                                onClick={() => setSelectedLead(lead)}
+                                            >
+                                                View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
+
+                    {/* Sidebar */}
+                    <aside className="w-full xl:w-96 shrink-0 flex flex-col justify-center items-center align-middle">
+                        <div className="sticky top-6  bg-(--bg-color) px-4 sm:px-6 py-6 sm:py-8 w-full sm:max-w-md border-2 border-gray-200 shadow-sm">
+                            <h3 className="text-center font-bold text-2xl">Student Info</h3>
+
+                            <table className="bg-(--bg-color) w-full shadow-sm mt-4">
+                                <thead className="bg-blue-100 border-b-2 border-gray-400">
+                                    <tr>
+                                        <th colSpan="2" className="text-left px-4 py-2 font-bold">
+                                            {selectedLead?.name || "Select a Lead"}
+                                        </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {leadFields.map(([label, value]) => (
+                                        <tr key={label}>
+                                            <th className="text-left px-4 py-2 font-semibold">{label}</th>
+                                            <td>{value || "-"}</td>
+                                        </tr>
+                                    ))}
+
+                                </tbody>
+                            </table>
+
+                            <div className="grid grid-cols-2 gap-3 mt-6">
+                                <a
+                                    href={`https://wa.me/${selectedLead?.phone}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="flex-1 bg-green-600 text-white py-2 rounded-lg text-center cursor-pointer"
+                                >
+                                    WhatsApp
+                                </a>
+
+                                <button
+                                    className="flex-1 bg-yellow-500 text-white py-2 rounded-lg cursor-pointer"
+                                >
+                                    Mark Contacted
+                                </button>
+
+                                <button
+                                    className="flex-1 bg-blue-500 text-white py-2 rounded-lg cursor-pointer"
+                                >
+                                    Mark Admitted
+                                </button>
+
+                                <button
+                                    className="flex-1 bg-red-600 text-white py-2 rounded-lg cursor-pointer"
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </aside>
                 </div>
             </section >
         </>
